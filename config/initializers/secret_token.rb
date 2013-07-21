3144b273267bc623bdae531ea16e300b8f9b990d
sweety <smehta1289@gmail.com>
2013-07-22 00:10:06 +0530

    active admin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc284::Application.config.secret_token = 'd442c83fd4879f581fe82be3a4cf68778ba7a9192fa7b55cad5e738ec4456a52d3538acb2e524a734d59a98744a1baaeb8f0c56ba880b6bf8c423c33651518cc'
